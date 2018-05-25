bf2fad9b44e96071356ed0df01d59f18294c818d
Abubakar Riaz <abubakar.riaz@outlook.com>
2018-05-25 11:46:40 +0500

    Added some comments

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -14,6 +14,9 @@ namespace Com.Mindbridge.EndeApp.GUI
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Main GUI form of app
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
@@ -24,30 +27,47 @@ namespace Com.Mindbridge.EndeApp.GUI
             _comboItems.Add(new Data { Name = "App2", Key = "MyLongComplexKey.123" });
             _comboItems.Add(new Data { Name = "App3", Key = "QfdfgWE<>LPO{><MM(*&$%%" });
 
+            // bind data to comboBox 
             comboBox1.DataSource = _comboItems;
             comboBox1.DisplayMember = "Name";
             comboBox1.ValueMember = "Key";
 
         }
 
+        /// <summary>
+        /// Encrypt text button
+        /// </summary>
         private void Encrypt_button_Click(object sender, EventArgs e)
         {
             Encrypt_EncryptedText_textBox.Text= new Encryption().EncryptText(Encrypt_PlainText_textBox.Text, _key_textBox.Text);   
         }
 
+        /// <summary>
+        /// Decrypt text button
+        /// </summary>
         private void Decrypt_button_Click(object sender, EventArgs e)
         {
            Decrypt_PlainText_textBox.Text= new Encryption().DecryptText(Decrypt_DecryptedText_textBox.Text, _key_textBox.Text);
         }
 
+        /// <summary>
+        /// The Key text box changes when the dropdown selection is changed
+        /// </summary>
         private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
         {
+            // Get hard coded dropdown list value and show it in 'key' text box
             _key_textBox.Text = comboBox1.SelectedValue.ToString();
         }
 
+        /// <summary>
+        /// When the form is completely loaded this function will execute
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Get the main app icon from saved resource folder
             Icon icon = Icon.ExtractAssociatedIcon("Resources/EnDe-App.ico");
+
+            // Assign icon to the icon property of the form
             this.Icon = icon;
         }
     }
